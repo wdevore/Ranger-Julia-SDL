47d9fd2cb1b6c220e161256165ad61c3ba906f0a
William Cleveland <william.quartz@gmail.com>
2019-03-22 10:30:11 -0400

    Add files via upload

---
+++ geometry/point.jl
@@ -0,0 +1,23 @@
+module Geometry
+
+import Base.copy
+
+export Point
+
+mutable struct Point{T <: AbstractFloat}
+    x::T
+    y::T
+
+    function Point{T}() where {T <: AbstractFloat}
+        new(0.0, 0.0)
+    end
+
+    function Point{T}(x::T, y::T) where {T <: AbstractFloat}
+        new(x, y)
+    end
+end
+
+# copy specializations
+copy(p::Point) = Point{Float64}(p.x, p.y)
+
+end
